b5dead12d095cdb5b08827879c5372cc016ac9a3
CaioAraujo <caioberardo.401@gmail.com>
2024-10-03 11:24:22 -0300

    criando funcao javascript

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // estrutura de repetição
 
 for(let i = 0; i < 10; i++){
-    console.log(i)
+//    console.log(i)
 }
 
 // while 
@@ -9,6 +9,26 @@ for(let i = 0; i < 10; i++){
 let f = 0 ;
 
 while(f<10){
-    console.log()
+//    console.log()
     f++
 }
+
+// do while 
+
+let w = 0 
+
+do{
+//    console.log(w);
+    w++
+}
+while(w < 21)
+
+//funções 
+
+function ola(nome){
+    // `` (strings literal ou template sting ${} chama a variavel)
+    //console.log(`seja bem vindo a ${nome}`)
+    //alert(`seja bem vindo a ${nome}`)
+    document.write(`seja bem vindo a ${nome}`)
+}
+ola("fiap")
